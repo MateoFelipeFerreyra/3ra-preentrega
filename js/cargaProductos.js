d4440805a31cd7c790c844f66f94753e5f8b713c
Mateo Felipe Ferreyra <mateofelipeferreyra@gmail.com>
2023-03-21 12:37:10 -0300

    no muestra las cards

---
+++ js/cargaProductos.js
@@ -0,0 +1,8 @@
+let listaProductos = [
+    { id: 1, nombre: "Cap America Funko Pop", precio: 6000, cantidad: 1 },
+    { id: 2, nombre: "Messi Funko Pop", precio: 10000, cantidad: 1 },
+    { id: 3, nombre: "Spiderman Funko Pop", precio: 6000, cantidad: 1 }
+]
+
+const arrEnformatoJSON = JSON.stringify(listaProductos)
+localStorage.setItem("listaProductos",arrEnformatoJSON)
--- js/main.js
+++ js/main.js
@@ -1,81 +1,89 @@
-class juguetes {
-    constructor(id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
-
-class Carrito {
+class productoController {
     constructor() {
-        this.listaCarrito = [];
+        this.listaProductos = [];
     }
-    agregar([juguetes, cantidad]) {
-        this.listaCarrito.push([juguetes, cantidad]);
+
+    subir() {
+        let obtenerListaJSON = localStorage.getItem("listaProductos")
+
+        if (obtenerListaJSON) {
+            this.listaProductos = JSON.parse(obtenerListaJSON)
+        }
     }
-    mostrar() {
-        let mostrarJuguetes = ""
-        console.log(this.listaCarrito)
-        this.listaCarrito.forEach(juguetes => {
-            mostrarJuguetes += " nombre: " + juguetes[0].nombre + " x" + juguetes[1] + " precio ind: $" + juguetes[0].precio + " precio TotInd: $" + juguetes[0].precio * juguetes[1] + "\n"
-        })
-        return mostrarJuguetes
-    }
-    calcularTotal() {
-        let total = 0
-        this.listaCarrito.forEach(juguetes => {
-            total += juguetes[0].precio * juguetes[1]
-        })
-        return total;
+    mostrarEnDOM(contenedor_productos) {
+        this.listaCarrito.forEach(producto => {
+            contenedor_productos.innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src ="./img/funko-img${producto.id}.jpg" class="card-img-top" alt = "...">
+            <div class="card-body" >
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">${producto.precio}</p>
+                <button id="comprar${producto.id}">
+                comprar
+                </button>
+            </div>
+    </div >`
+        });
     }
 }
-
-class productControler {
+class carritoController {
     constructor() {
-        this.listaJuguetes = []
+        this.listaCarrito = []
     }
-    agregar(objJuguete) {
-        this.listaJuguetes.push(objJuguete)
+    subir() {
+        let obtenerListaJSON = localStorage.getItem("listaCarrito")
+
+        if (obtenerListaJSON) {
+            this.listaCarrito = JSON.parse(obtenerListaJSON)
+        }
     }
-    existe(id) {
-        return this.listaJuguetes.some(juguetes => juguetes.id == id)
+
+    anadir(producto) {
+        this.listaCarrito.push(producto);
+        let arrformatoJSON = JSON.parse(this.listaCarrito)
+        localStorage.setItem("listaCarrito",arrformatoJSON)
     }
-    buscar(id) {
-        return this.listaJuguetes.find(juguetes => juguetes.id == id)
-    }
-    mostrar() {
-        let mostrarJuguetes = ""
-        this.listaJuguetes.forEach(juguetes => {
-            mostrarJuguetes += "id: " + juguetes.id + " nombre: " + juguetes.nombre + " precio: $" + juguetes.precio + "\n"
-        })
-        return mostrarJuguetes
+
+    mostrarEnDOM(contenedor_carrito) {
+        contenedor_carrito.innerHTML= ""
+        this.listaCarrito.forEach(producto => {
+            contenedor_carrito.innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src ="./img/funko-img${producto.id}.jpg" class="card-img-top" alt = "...">
+            <div class="card-body" >
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">${producto.precio}</p>
+                <button id="comprar${producto.id}">
+                comprar
+                </button>
+            </div>
+    </div >`
+        });
     }
 }
 
-const carritoCompra = new Carrito();
-const controladorProducto = new productControler();
+const controladorProductos = new productoController()
+const controladorCarrito = new carritoController()
 
-controladorProducto.agregar(new juguetes(1, "Cap America Funko Pop", 15000))
-controladorProducto.agregar(new juguetes(2, "Messi Funko Pop", 5000))
-controladorProducto.agregar(new juguetes(3, "Spiderman Funko Pop", 10000))
+controladorProductos.subir()
+controladorCarrito.subir()
+
+//DOM
+const contenedor_productos = document.getElementById(contenedor_productos)
+const contenedor_carrito = document.getElementById(contenedor_carrito)
+
+//app
+controladorProductos.mostrarEnDOM(contenedor_productos)
+controladorCarrito.mostrarEnDOM(contenedor_carrito)
 
 
-const contenedor_productos = document.getElementById("contenedor_productos")
-// contenedor_productos.innerHTML =
-controladorProducto.listaJuguetes.forEach(producto =>{product = document.createElement('div'),
-product.innerHTML = (`<div class="card" style="width: 18rem;">
-    <img src ="./img/funko-img${producto.id}.jpg" class="card-img-top" alt = "...">
-        <div class="card-body" >
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.precio}</p>
-            <button id="boton">
-            comprar
-            </button>
-        </div>
-</div >`),
-boton = document.getElementById('boton')
-boton.addEventListener('click',() => { 
-    carritoCompra.agregar(producto,1) 
+controladorProductos.listaProductos.forEach( producto => {
+    const productoQueSeAnade = document.getElementById(`Funko${producto.id}`)
+
+    productoQueSeAnade.addEventListener("click",()=>{
+        controladorCarrito.anadir(producto)
+        contenedor_carrito.innerHTML = ""
+        controladorCarrito.mostrarEnDOM(contenedor_carrito)
+    })
 })
-contenedor_productos.append(product)})
-console.log(carritoCompra.listaCarrito)
+
