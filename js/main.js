173c3c7afa0c2476666e49c82d4c93ddbf33744e
Mateo Felipe Ferreyra <mateofelipeferreyra@gmail.com>
2023-04-11 00:10:43 -0300

    ultima entrega

--- js/main.js
+++ js/main.js
@@ -42,25 +42,45 @@ class carritoController {
         let arrformatoJSON = JSON.stringify(this.listaCarrito)
         localStorage.setItem("listaCarrito", arrformatoJSON)
     }
+    borrar(posicionDelObjetoABorrar) {
+        this.listaCarrito.splice(posicionDelObjetoABorrar, 1);
+    }
 
     mostrarEnDOM(contenedor_carrito) {
-        contenedor_carrito.innerHTML = ""
-        this.listaCarrito.forEach(producto => {
+        contenedor_carrito.innerHTML = "";
+        for (let i = 0; i < this.listaCarrito.length; i++) {
             contenedor_carrito.innerHTML += `
-    <div class="card" style="width: 18rem;">
-        <img src ="./img/funko-img${producto.id}.jpg" class="card-img-top" alt = "...">
-            <div class="card-body" >
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">$${producto.precio}</p>
-            </div>
-    </div >`
-        });
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src ="./img/funko-img${this.listaCarrito[i].id}.jpg" class="card-img-top" alt = "...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bolder border-bottom-0">${this.listaCarrito[i].nombre}</h5>
+                                <p class="card-text">cantidad: x ${this.listaCarrito[i].cantidad} unidades</p>
+                                <button id="borrar${i}" class="buttonTrash"><i class="fas fa-trash-alt"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            document
+                .getElementById(`borrar${i}`)
+                .addEventListener("click", () => {
+                    this.borrar(i);
+                    localStorage.setItem(
+                        "listaCarrito",
+                        JSON.stringify(this.listaCarrito)
+                    );
+                    this.mostrarEnDOM(contenedor_carrito);
+                });
+        }
         const total = this.listaCarrito.reduce((acc, el) => acc + el.precio, 0);
         const totalFinal = document.getElementById("totalFinal")
         console.log(total)
         totalFinal.innerText = `Total: $ ${total}`
     }
-
     limpiar() {
         this.listaCarrito = []
         localStorage.removeItem("listaCarrito")
@@ -85,11 +105,11 @@ controladorCarrito.mostrarEnDOM(contenedor_carrito)
 
 controladorProductos.listaProductos.forEach(producto => {
     const productoQueSeAnade = document.getElementById(`funko${producto.id}`)
-
     productoQueSeAnade.addEventListener("click", () => {
         controladorCarrito.anadir(producto)
         controladorCarrito.subir()
         controladorCarrito.mostrarEnDOM(contenedor_carrito)
+        console.log(controladorCarrito.listaCarrito)
     })
 })
 
@@ -107,12 +127,12 @@ terminarCompra.addEventListener("click", () => {
             timer: 1500
         })
 
-    }else{
+    } else {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'NO HAY PRODUCTOS EN TU CARRITO',
-          })
+        })
     }
 
 })
